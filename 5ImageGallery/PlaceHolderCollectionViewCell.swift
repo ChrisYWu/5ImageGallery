4f2ce67300089caa24bdf6cf0d64864fea7b3c00
Chris Wu <chris.wu@live.com>
2019-01-06 16:53:28 -0600

    5 Image Gallery Requirement 1~9 completed

---
+++ 5ImageGallery/PlaceHolderCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceHolderCollectionViewCell.swift
+//  5ImageGallery
+//
+//  Created by Chris Wu on 1/6/19.
+//  Copyright © 2019 Wu Personal Team. All rights reserved.
+//
+
+import UIKit
+
+class PlaceHolderCollectionViewCell: UICollectionViewCell {
+    
+}
